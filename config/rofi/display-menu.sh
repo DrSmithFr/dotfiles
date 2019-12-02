40bc4a96c6fa52eb0e377832aa9f1bb9ac43c631
John Daligault <johndaligault@gmail.com>
2019-12-02 09:27:22 +0100

    configuration with screen left

--- config/rofi/display-menu.sh
+++ config/rofi/display-menu.sh
@@ -3,7 +3,7 @@
 EDP=$(xrandr | grep eDP | cut -d' ' -f1)
 HDMI=$(xrandr | grep HDMI | cut -d' ' -f1)
 
-OPTIONS="(1) Above\n(2) Right\n(3) Left\n(4) None"
+OPTIONS="(1) Left\n(2) Above\n(3) Right\n(4) None"
 option=`echo -e $OPTIONS | awk '{print $1}' | tr -d '\r\n\t'`
 
 selected=$@
@@ -11,17 +11,17 @@ selected=$@
 if [ "$selected" ]
 then
   case $selected in
-    1|*Above)
+    1|*Left)
+      notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Left"
+      xrandr --output $HDMI --left-of $EDP --auto
+      ;;
+    2|*Above)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Above"
       xrandr --output $HDMI --above $EDP --auto
       ;;
-    2|*Right)
+    3|*Right)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Right"
       xrandr --output $HDMI --right-of $EDP --auto
-      ;;
-    3|*Left)
-      notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Left"
-      xrandr --output $HDMI --left-of $EDP --auto
       ;;
     4|*None)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "disabled"
