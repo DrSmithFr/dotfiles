b4517260b5d70cde95e652682d357a714a8596e0
John Daligault <johndaligault@gmail.com>
2020-02-06 11:03:13 +0100

    updating to ~ monitor config

--- config/rofi/display-menu.sh
+++ config/rofi/display-menu.sh
@@ -2,6 +2,7 @@
 
 EDP=$(xrandr | grep eDP | cut -d' ' -f1)
 HDMI=$(xrandr | grep HDMI | cut -d' ' -f1)
+DVI=$(xrandr | grep DVI | cut -d' ' -f1)
 
 OPTIONS="(1) Left\n(2) Above\n(3) Right\n(4) None"
 option=`echo -e $OPTIONS | awk '{print $1}' | tr -d '\r\n\t'`
@@ -14,18 +15,22 @@ then
     1|*Left)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Left"
       xrandr --output $HDMI --left-of $EDP --auto
+      xrandr --output $DVI --left-of $HDMI --auto
       ;;
     2|*Above)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Above"
       xrandr --output $HDMI --above $EDP --auto
+      xrandr --output $DVI --above $HDMI --auto
       ;;
     3|*Right)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "enabled - Right"
       xrandr --output $HDMI --right-of $EDP --auto
+      xrandr --output $DVI --right-of $HDMI --auto
       ;;
     4|*None)
       notify-send --urgency=critical --app-name=HDMI -t 3000 "disabled"
       xrandr --output $HDMI --off
+      xrandr --output $DVI --off
       ;;
   esac
 else
